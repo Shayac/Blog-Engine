e0fcb968e8d53833b7e07b22850bf1ff2dca4708
Shaya Cohen <shayac1@gmail.com>
2014-01-09 15:55:36 -0500

    adding

---
+++ Blog/Models/AllBlogPostsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Blog.Entities;
+
+namespace Blog.Models
+{
+    public class AllBlogPostsViewModel
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public string ChopOffPost(BlogPost post)
+        {
+            int lastSpace = post.PostContent.LastIndexOf(" ", 100);
+            return post.PostContent.Substring(0, (lastSpace > 0) ? lastSpace : 100);
+        }
+    }
+}
